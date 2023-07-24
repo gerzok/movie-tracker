7b530e030c4514c3a7dc7ea79fdcd64ca0f62224
Gerzo Karim <gerzok@devhouse.io>
2023-07-24 00:34:17 -0600

    feat: added movie review accordion

--- your-code-here/app.js
+++ your-code-here/app.js
@@ -5,13 +5,42 @@ import "./tests.js";
 const { css } = emotion;
 const { useEffect, useState } = React;
 
-const style = css`
-  text-align: center;
-`;
+const style = {
+  container: css`
+  margin: 0 auto;
+    width: 45vw;
+  `,
+
+  li: css`
+    &:hover {
+      background-color: #DDD;
+      cursor: pointer;
+    }
+  `,
+
+  liSelected: css`
+    background-color: #DDD;
+    padding: 10px;
+  `,
+};
+
+const accordion = {
+  active: css`
+    background-color: #DDD;
+    padding: 10px 20px;
+    display: block;
+  `,
+  inactive: css`
+    display: none;
+  `
+};
 
 export const App = ({ onLoad }) => {
   const [movies, setMovies] = useState([]);
+  const [reviews, setReviews] = useState([]);
   const [decades, setDecades] = useState([]);
+  const [toggleAccordion, setToggleAccordion] = useState(false);
+  const [contentReview, setContentReview] = useState("");
   const [selectedDecade, setSelectedDecade] = useState("");
 
   useEffect(onLoad, []); // to run tests
@@ -24,6 +53,13 @@ export const App = ({ onLoad }) => {
     getDecadesFromMovies();
   }, [movies]);
 
+  const getMoviesReview =  async () => {
+    const fetchReviews = await fetch('/api/reviews.json');
+    const reviews = await fetchReviews.json();
+    setReviews(reviews);
+    localStorage.setItem('reviews', JSON.stringify(reviews));
+  };
+
   const getMovies = async () => {
     const fetchMovies = await fetch('/api/movies.json');
     const movies = await fetchMovies.json();
@@ -35,12 +71,17 @@ export const App = ({ onLoad }) => {
 
   const localStorageCache = () => {
     const moviesFromLocalStorage = localStorage.getItem('movies');
+    const reviewsFromLocalStorage = localStorage.getItem('reviews');
 
-    if (moviesFromLocalStorage !== null) {
+    if (moviesFromLocalStorage !== null && reviewsFromLocalStorage !== null) {
       const parseLocalStorageMovies = JSON.parse(moviesFromLocalStorage);
+      const parseLocalStorageReviews = JSON.parse(reviewsFromLocalStorage);
+
       setMovies(parseLocalStorageMovies);
+      setReviews(parseLocalStorageReviews);
     } else {
       getMovies();
+      getMoviesReview();
     }
   };
 
@@ -89,32 +130,47 @@ export const App = ({ onLoad }) => {
     }
   };
 
-  console.log('movies', movies);
-  console.log('selectedDecade', selectedDecade);
+  const handleAccordion = (event, movieId) => { 
+    if (event.target.localName === "a") {
+      return;
+    }
+
+    const toggle = toggleAccordion === movieId ? false : movieId;
+    const getReviewToShow = reviews.find((review) => review['movie-id'] === movieId);
+
+    setContentReview(getReviewToShow.review);
+    setToggleAccordion(toggle);
+  };
+
 
   return html`
-  <div>
-    <h1>Movies Evan Likes!</h1>
-    <p>Below is a (not) comprehensive list of movies that Evan really likes.</p>
-    <hr />
+    <div className=${style.container}>
+      <h1>Movies Evan Likes!</h1>
+      <p>Below is a (not) comprehensive list of movies that Evan really likes.</p>
+      <hr />
 
-    <fieldset>
-      <label>Title contains:</label>
-      <input name="search" type="text" onChange=${(e) => handleSearch(e.target.value)} placeholder="Search by title" />
-    </fieldset>
-    <fieldset>
-      <label>Decade:</label>
-      <select onChange=${(e) => handleFilterByDecade(e.target.value)}>
-        <option value="spacer"></option>
-        ${decades.map((decade) => html`<option key=${decade} value=${decade}>${decade}</option>`)}
-      </select>
-    </fieldset>
+      <fieldset>
+        <label>Title contains:</label>
+        <input name="search" type="text" onChange=${(e) => handleSearch(e.target.value)} placeholder="Search by title" />
+      </fieldset>
+      <fieldset>
+        <label>Decade:</label>
+        <select onChange=${(e) => handleFilterByDecade(e.target.value)}>
+          <option value="spacer"></option>
+          ${decades.map((decade) => html`<option key=${decade} value=${decade}>${decade}</option>`)}
+        </select>
+      </fieldset>
 
-    <ul>
-      ${movies.map(movie => html`
-        <li key=${movie.id}><span>${movie.score * 100}%</span> <a href=${movie.url}>${movie.title}</a> <span>(${movie.year})</span></li>
-      `)}
-    </ul>
-  </div>
+      <ul>
+        ${movies.map(movie => html`
+          <li key=${movie.id} className=${movie.id !== toggleAccordion ? style.li : style.liSelected}>
+            <div onClick=${(e) => handleAccordion(e, movie.id)}>
+              <span>${movie.score * 100}%</span> <a href=${movie.url}>${movie.title}</a> <span>(${movie.year})</span>
+              <div className=${movie.id === toggleAccordion ? accordion.active : accordion.inactive}>${contentReview}</div>
+            </div>
+          </li>
+        `)}
+      </ul>
+    </div>
   `;
 };
